850ad1a6ed83c56e0f4153a5e47b7d981c397c2b
Jose Maria Sosa Gomez <jose.sosa1@ibm.com>
2021-03-14 18:50:52 -0500

    IAM Group and Outputs

--- my_first_project/my_first_project_stack.py
+++ my_first_project/my_first_project_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_s3 as _s3,
+    aws_iam as _iam,
     core
 )
 
@@ -14,6 +15,32 @@ class MyFirstProjectStack(core.Stack):
             self,
             "myBucketId",
             bucket_name="mycdkbucketsosa120483001000",
-            versioned=True,
-            encryption=_s3.BucketEncryption.KMS_MANAGED
+            versioned=False,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketId1"
+        )
+
+        snstopicname="abcdzys123"
+
+        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
+            raise ValueError("Maximum value can be only 10 characters")
+
+        print(mybucket.bucket_name)
+
+        _iam.Group(
+            self,
+            "gid"
+        )
+
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name,
+            description=f"My First CDK Bucket",
+            export_name="myBucketOutput1"
         )
